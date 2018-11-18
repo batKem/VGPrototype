99db2a3a9f28bf61625eb320e402b6ef2e0d32bd
Mohamed Khadri <mohamed.khadri@epfl.ch>
2018-11-18 19:12:46 +0100

    uploading code (messed up previous commit)

---
+++ VGPrototype/New Unity Project/Assets/Code/Main.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Positionable p = new Positionable(new Vector2(1,1),null);
+		Actor a = new Actor (new Vector2(50,50), null);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
